cf4003a36090e466e907cdb2af890c9e659e941c
willerhe <willerhe@126.com>
2020-03-05 08:59:45 +0800

    添加商城页面

--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -2,6 +2,20 @@
   <div>
     <Header/>
     <div class="content">
+      <div style="padding: 40px 10%;display: flex;justify-content: center;align-items: center">
+            <div style="display: flex;justify-content: start;align-items: center;flex-flow: wrap">
+              <el-card :body-style="{ padding: '0px' }"  style="width: 25%;margin: 10px" v-for="(o, index) in 8" :key="o" :offset="index > 0 ? 2 : 0">
+                <img src="@/assets/store1.png" class="image">
+                <div style="padding: 14px;">
+                  <span>精品狗粮进口500g全国门店有售</span>
+                  <div class="bottom clearfix">
+                    <span style="color: red">￥ 188元</span>
+                    <el-button type="text" class="button" @click="toDetail">查看详情</el-button>
+                  </div>
+                </div>
+              </el-card>
+            </div>
+      </div>
 
     </div>
     <Footer/>
@@ -14,12 +28,52 @@
 
   export default {
     name: "Store",
-    components: {Footer, Header}
+    components: {Footer, Header},
+    data(){
+      return{
+        currentDate: new Date()
+      }
+    },
+    methods:{
+      toDetail(){
+        alert('查看详情')
+      }
+    }
   }
 </script>
 
 <style scoped>
   .content {
     min-height: 70vh;
+  }
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    max-height: 300px;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both
   }
 </style>
